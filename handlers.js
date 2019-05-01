3db944dc375e23d6504d704dc0dc0b2414ed008a
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-05-01 23:19:27 +0200

    add POST request handling

--- handlers.js
+++ handlers.js
@@ -1,18 +1,27 @@
-var exec = require('child_process').exec;
+var querystring = require('querystring');
 
-function start(res) {
-  console.log('Handler "start" was called.');
-  exec('ls -lah', function (err, stdout, stderr) {
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.write(stdout);
-    res.end();
-  });
+function start(res, postData) {
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write(body);
+  res.end();
 }
 
-function upload(res) {
-  console.log('Handler "upload" was called.');
+function upload(res, postData) {
   res.writeHead(200, {'Content-type': 'text/plain'});
-  res.write('Hello Upload.');
+  var output = querystring.parse(postData).text;
+  res.write(output);
   res.end();
 }
 
--- server.js
+++ server.js
@@ -3,10 +3,22 @@ var url = require('url');
 
 function start(route, handle) {
   http.createServer(function(req, res) {
+    var postData = '';
     var pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(handle, pathname, res);
+    req.setEncoding("utf8");
+
+    // Listening to POST request & getting data
+    req.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('postDataChunk: ' + postDataChunk);
+    });
+
+    // POST end event passing postData to route
+    req.addListener('end', function() {
+      route(handle, pathname, res, postData);
+    });
 
   }).listen(8000);
 }
